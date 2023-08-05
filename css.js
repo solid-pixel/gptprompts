d90a417b9c617932b8a604a0c78168b131be0dc0
Alessandro Benassi <plasmax@gmail.com>
2023-08-05 16:19:29 +0200

    commit

--- css.js
+++ css.js
@@ -1,1 +1,21 @@
+/**
+ * @name css.js
+ * @version 0.1.0
+ * @url https://github.com/solid-pixel/gptprompts/blob/main/css.js
+ */
 
+// inject CSS in the page
+
+let css = `
+    body {
+        background-color: #eee;
+        color: #333;
+        font-family: Arial, sans-serif;
+    }
+`
+
+function injectStyle(css) {
+    const style = document.createElement('style');
+    style.innerHTML = css;
+    document.head.appendChild(style);
+}
